9064ee1eb95286ac6074a426904e340752c838c5
akvgit <thearunkv@gmail.com>
2014-08-05 18:11:56 +0530

    Update tran.js
    Changes done so that  content comes from english language file if there is no value for key.

--- tran.js
+++ tran.js
@@ -1,68 +1,110 @@
-     $(document).ready(function () {
-        //Call the translate() method
+var defaultLangFile     = "en-us.json",
+    arrDefaultLang      = [],
+    defaultLangValues   = {};
+
+    //Call the translate() method
+    $(document).ready(function () {
         tran.translate();
-    }); 
+    });
 
     var tran = {
+        //Load default language file(english)
+        getDefaultFile: function () {
+            return $.getJSON(defaultLangFile).pipe(function (defaultContents, status, jqXHR) {
+                return defaultContents;
+            });
+        },
+
+        //Store the contents of default language file in an array
+        storeDefaultFileContents: function (defaultContents) {
+            arrDefaultLang = defaultContents;
+            return arrDefaultLang;
+        },
+
+        //Translate language
         translate: function () {
-            var activeLanguage = 'hi-in',
-                jsonLangFile = activeLanguage + '.json';
+            var activeLangFile = 'hi-in.json';
+            
+            //Call the ajax function to load the json lang file
+            var jqxhr = $.getJSON(activeLangFile);
 
-            var jqxhr = $.getJSON(jsonLangFile)
-
-               .done(function (data) {
-                   tran.populateText(data);
-               })//end done
+            //Done function for successful response
+            jqxhr.done(function (data) {
+                // Call the function populateText to set the language text
+                tran.populateText(data);
+            }); //end done
 
             //Handle failures
-              .fail(function () {
-                  console.log("Oops!! There was an error in processing the request");
-              }); //end fail and this also ends the getJSON method
+            jqxhr.fail(function () {
+                console.log("Oops!! There was an error in processing the request");
+            }); //end fail
 
-        }, //translate function ends
+        }, //end translate
 
+        //Populate the language text
         populateText: function (data) {
             $("[data-tran]").each(function () {
-                var parentElement 		= $(this),
-            		    key 		= parentElement.attr("data-tran"),
-            		    elementType 	= '',
-                            derivedElementType  = '';
+                var parentElement = $(this),
+            		    key = parentElement.attr("data-tran"),
+            		    elementType = '',
+                        derivedElementType = '',
+                        labelText = '';
                 /* 
                 For all html elements of type input
                 */
                 if (parentElement.prop('tagName').toLowerCase() == 'input') {
                     elementType = parentElement.prop('type').toLowerCase();
                     derivedElementType = (elementType == 'button' || elementType == 'submit') ? 'value' : 'placeholder';
-
-                    //If the value for the key does not exist in the language file, the key name is returned
+                    /*
+                    If the value for the key does not exist in the selected language file,
+                    get the contents from the default language file(english)
+                    */
                     if (data[key] == undefined) {
-                        parentElement.prop(derivedElementType, key);
-                    }
+                        
+                        var fnGetDefaultFile = tran.getDefaultFile(),
+                            defaultLangValues = {};
 
-                    //Get the value of key from the language file
-                    else parentElement.prop(derivedElementType, data[key]);
+                        fnGetDefaultFile.done(function (defaultContents) {
+                            defaultLangValues = tran.storeDefaultFileContents(defaultContents);
+
+                           //Get the value if the key exists, else set it to key
+                            labelText = (defaultLangValues[key] == undefined) ? key : defaultLangValues[key];
+                            parentElement.prop(derivedElementType, labelText);
+                        });// end done function
+                    }
+                    else {
+                        labelText = data[key];
+                        parentElement.prop(derivedElementType, labelText);
+                    }
                 }
                 /*
                 For all other html elements
                 */
                 else {
-                    parentElement.text(data[key]);
+                    /*
+                    If the value for the key does not exist in the selected language file,
+                    get the contents from the default language file(english)
+                    */
+                    if (data[key] == undefined) {
+                        var fnGetDefaultFile = tran.getDefaultFile(),
+                            defaultLangValues = {};
+
+                        fnGetDefaultFile.done(function (defaultContents) {
+                            defaultLangValues = tran.storeDefaultFileContents(defaultContents);
+
+                            //Get the value if the key exists, else set it to key
+                            labelText = (defaultLangValues[key] == undefined) ? key : defaultLangValues[key];
+                            parentElement.text(labelText);
+                        });
+                    }
+                    /*
+                     Populate the language text from the selected language file
+                    */
+                    else {
+                        labelText = data[key];
+                        parentElement.text(labelText);
+                    } 
                 }
-            })//$.each ends
-        } //populateText function ends
+            })// end each
+        } //end populateText
     };
-
-
-
-
-/*
-
-
-*/
-
-
-
-
-
-
-
